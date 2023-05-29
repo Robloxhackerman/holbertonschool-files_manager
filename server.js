66a77409489feb5a201d89ad014f297e2696745f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-29 20:16:01 -0300

    task2

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import router from './routes/index';
+
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 
 router(app);
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`);
 });
