b4e86e16989ee7cd4310ccf1acf80b9a1d2fd61a
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-29 20:18:46 -0300

    task2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 import AppController from '../controllers/AppController';
- const express = require('express');
 
- const router = (app) => {
+const express = require('express');
+
+const router = (app) => {
   const path = express.Router();
   app.use(express.json());
   app.use('/', path);
 
   path.get('/status', ((request, response) => AppController.getStatus(request, response)));
   path.get('/stats', ((request, response) => AppController.getStats(request, response)));
- }
+};
 
- export default router;
+export default router;
