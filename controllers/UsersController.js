a4bb736932d0f3b77ca5d79d080c27b5aecd3a38
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-29 20:55:19 -0300

    task3 xd

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,32 +4,32 @@ import sha1 from "sha1";
 
 class UsersController {
 
-  static postNew(request, response) {
+  static async postNew(request, response) {
     const userQueue = new Bull('userQueue');
     const email = request.body.email;
 
     if (!email) {
-      return response.status(400).send({ error: 'Missing email' });
+      return response.status(400).send({error: 'Missing email'});
     }
 
     const password = request.body.password;
     if (!password) {
-      return response.status(400).send({ error: 'Missing password' });
+      return response.status(400).send({error: 'Missing password'});
     }
 
-    const emailExists = DBClient.db.collection('users').findOne({ email: email });
+    const emailExists = await DBClient.db.collection('users').findOne({email: email});
     if (emailExists) {
-      return response.status(400).send({ error: 'Already exist' });
+      return response.status(400).send({error: 'Already exist'});
     }
 
     const userPassword = sha1(password);
-    const result = DBClient.db.collection('users').insertOne({ email: email, password: userPassword });
+    const result = await DBClient.db.collection('users').insertOne({email: email, password: userPassword});
 
     userQueue.add({
       userId: result.insertedId,
     });
 
-    return response.status(201).send({ id: result.insertedId, email: email });
+    return response.status(201).send({id: result.insertedId, email: email});
   }
 
 }
