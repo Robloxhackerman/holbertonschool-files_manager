658e4c8facf656fe820b058f2cf5b3a5dd80abb9
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-05-29 21:25:23 -0300

    task4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 import sha1 from 'sha1';
 import DBClient from '../utils/db';
+import {RedisClient} from "redis";
 
 const Bull = require('bull');
 
@@ -30,6 +31,20 @@ class UsersController {
     });
 
     return response.status(201).send({ id: result.insertedId, email });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+    if (!token) return response.status(401).send({ error: 'Unauthorized' });
+
+    const redisToken = await RedisClient.get(`auth_${token}`);
+    if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
+
+    const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    delete user.password;
+
+    return response.status(200).send({ id: user._id, email: user.email });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,11 @@ const router = (app) => {
 
   path.get('/status', ((request, response) => UsersController.getStatus(request, response)));
   path.get('/stats', ((request, response) => UsersController.getStats(request, response)));
+
   path.post('/users', ((request, response) => UsersController.postNew(request, response)));
+
+  path.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
+  path.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
 };
 
 export default router;
